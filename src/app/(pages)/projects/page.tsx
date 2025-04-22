c974ed6caa90f2ebf29a545d26d2a6b20ea2b366
ragipidavid <ragipidavid@gmail.com>
2025-04-21 19:12:00 -0700

    Changed link

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export const metadata = {
-    title: 'Projects',
-    description: 'Explore David Ragipi\'s software engineering projects and portfolio work.',
+  title: "Projects",
+  description:
+    "Explore David Ragipi's software engineering projects and portfolio work.",
 };
 
 const ProjectsPage = () => {
-    return (
-        <main>
-            <div className="md:pt-32 md:pl-40 max-w-4xl">
-                <h1 className="text-6xl font-medium mb-12">Projects</h1>
+  return (
+    <main>
+      <div className="md:pt-32 md:pl-40 max-w-4xl">
+        <h1 className="text-6xl font-medium mb-12">Projects</h1>
 
-                <div className="space-y-8 text-xl">
-                    <p>
-                        Here are some of the projects I&apos;ve worked on.
-                    </p>
+        <div className="space-y-8 text-xl">
+          <p>Here are some of the projects I&apos;ve worked on.</p>
 
-                    <div className="space-y-4">
-                        <div>
-                            <h2 className="text-2xl mb-2">Interview Buddy</h2>
-                            <p><a href="https://interviewaibuddy.com" className="text-gray-500 hover:underline">interviewaibuddy.com</a></p>
-                        </div>
-                    </div>
-                </div>
+          <div className="space-y-4">
+            <div>
+              <h2 className="text-2xl mb-2">Interview Buddy</h2>
+              <p>
+                <a
+                  href="https://app.interviewaibuddy.com"
+                  className="text-gray-500 hover:underline"
+                >
+                  interviewaibuddy.com
+                </a>
+              </p>
             </div>
-        </main>
-    );
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ProjectsPage;
